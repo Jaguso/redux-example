cebda836dc8c10cb2fe9a1cbefac8af170881279
Jaguso <jagutzo@gmail.com>
2019-03-26 17:19:23 -0600

    made basic redux example

--- src/index.js
+++ src/index.js
@@ -4,9 +4,42 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const reducer = (state, action)  => {
+  switch (action.type) {
+    case "ADD":
+      state = state + action.payload;
+      break;
+    case "MULTIPLY":
+      state = state * action.payload;
+      break;
+    case "SUBTRACT":
+      state = state - action.payload;
+      break;
+  }
+  return state;
+};
+
+const store = createStore(reducer, 2);
+
+store.subscribe(() => {
+  console.log("Store updated", store.getState());
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: 10
+});
+
+store.dispatch({
+  type: "MULTIPLY",
+  payload: 3
+});
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
