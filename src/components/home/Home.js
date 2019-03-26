aa2703fd118fc1d72ae066c3018acd645d20469b
Jaguso <jagutzo@gmail.com>
2019-03-26 17:52:41 -0600

    added bootstrap

---
+++ src/components/home/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            row1 col1
+          </div>
+          <div className="col">
+            row1 col2
+          </div>
+        </div>
+        <h3>woiefj</h3>
+      </div>
+    );
+  }
+}
+
+export default Home;
