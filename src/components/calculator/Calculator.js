b474b6a5af982bb49c8ddcd0a659197e927be8ad
Jaguso <jagutzo@gmail.com>
2019-03-28 12:21:50 -0600

    price calculator works

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import './Calculator.css';
 
 class Calculator extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      price: "",
+      discount: ""
+    }
+  }
+
+  onChangeInput = (event) => {
+    const {name, value} = event.target;
+    console.log("Valores ", name, value)
+    this.setState({[name]: value});
+  }
+
+  calculatePrice = (price, discount) => {
+    return parseInt(price) * (parseInt(discount)/100);
+  }
+
   render() {
     return (
       <div className="container calculator">
@@ -9,17 +28,26 @@ class Calculator extends Component {
           <div className="col">
             <label>
               Price:
-              <input/>
+              <input type="text" name="price" className="form-control"
+              value={this.state.price} onChange={this.onChangeInput}
+              placeholder="$" />
             </label>
           </div>
           <div className="col">
             <label>
               Discount:
-              <input/>
+              <input type="text" name="discount" className="form-control"
+              value={this.state.discount} onChange={this.onChangeInput} 
+              placeholder="%" />
             </label>
           </div>
           <div className="col">
             Total price: 
+            {
+              this.state.price && this.state.discount ? 
+                <p>$ {this.calculatePrice(this.state.price, this.state.discount)}</p>
+                : <p>$</p>
+            }
           </div>
         </div>
           
