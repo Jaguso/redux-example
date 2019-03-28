bbc6ef2336a5c1de0cfd10cd665b4c5d91e09f72
Jaguso <jagutzo@gmail.com>
2019-03-28 13:27:56 -0600

    made inputs

--- src/components/calculatorRedux/CalculatorRedux.js
+++ src/components/calculatorRedux/CalculatorRedux.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 
 class CalculatorRedux extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      price: "",
+      discount: ""
+    }
+  }
+
+  onChangeInput = (event) => {
+    const {name, value} = event.target;
+    console.log("Valores: ", name, value);
+    this.setState({[name]: value});
+  }
+
   render() {
     return (
       <div className="container">
@@ -8,13 +23,17 @@ class CalculatorRedux extends Component {
           <div className="col">
             <label>
               Price:
-              <input className="form-control" />
+              <input className="form-control" type="text" name="price"
+              value={this.state.price} onChange={this.onChangeInput}
+              placeholder="$" />
             </label>
           </div>
           <div className="col">
             <label>
               Discount:
-              <input className="form-control"/>
+              <input className="form-control" type="text" name="discount"
+              value={this.state.discount} onChange={this.onChangeInput}
+              placeholder="%" />
             </label>
           </div>
           <div className="col">
