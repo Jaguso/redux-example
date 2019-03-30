547c4c89ab9a62bd0380b40e0a918396df13b5c3
Jaguso <jagutzo@gmail.com>
2019-03-30 14:11:00 -0600

    added styles on top

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 
-import Home from './components/home/Home';
 import Calculator from './components/calculator/Calculator';
 import CalculatorRedux from './components/calculatorRedux/CalculatorRedux';
 import Colors from './components/colors/Colors';
@@ -12,10 +11,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h3>My name is: {this.props.user.name}</h3>
-        <h3>My age is: {this.props.user.age}</h3>
-        <button onClick={() => this.props.setName('Luis')}>change name</button>
-        <button onClick={()=>this.props.setAge('12')}>change age</button>
+        <div style={{textAlign: 'center', margin: '30px'}}>
+          <h3>My name is: {this.props.user.name}</h3>
+          <h3>My age is: {this.props.user.age}</h3>
+          <button onClick={() => this.props.setName('Luis')} className="btn btn-primary" style={{margin: '8px'}}>change name</button>
+          <button onClick={()=>this.props.setAge('12')} className="btn btn-primary" style={{margin: '8px'}}>change age</button>
+        </div>
         <Calculator/>
         <CalculatorRedux />
         <Colors/>
